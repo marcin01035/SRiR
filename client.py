011828d335d46b3724ee2b468f1206715cc4b293
marcin01035 <45914959+marcin01035@users.noreply.github.com>
2019-01-20 11:27:58 +0100

    aktualizacja

--- client.py
+++ client.py
@@ -5,163 +5,153 @@ import Pyro4.utils.flame
 
 
 class Rmi():
-	#pola
+    # pola
 
-	src_file = None
-	dest_name = None
+    src_file = None
+    dest_name = None
 
-	#funkcje
+    # funkcje
 
-	def __init__(self):
-		self.src_file = ''
-		self.dest_name = ''
+    def __init__(self):
+        self.src_file = ''
+        self.dest_name = ''
 
+    def numberoflines(self, filename):
+        number = 0
+        with open(filename, 'r') as f:
+            for line in f:
+                number += 1
+        return number
 
-	def zmienne(self, source_file=None,dest_name=None):
-		if source_file is None:
-			self.src_file = input("Wprowadz nazwe pliku zrodlowego z rozszerzeniem: ")
-		else:
-			self.src_file = source_file
+    def zmienne(self, source_file=None, dest_name=None):
+        if source_file is None:
+            self.src_file = input("Wprowadz nazwe pliku zrodlowego: ")
+        else:
+            self.src_file = source_file
 
-		if dest_name is None:
-			self.dest_name = input("Wprowadz nazwe pliku docelowego z rozszerzeniem: ")
-		else:
-			self.dest_name = dest_name
+        if dest_name is None:
+            self.dest_name = input("Wprowadz nazwe pliku docelowego: ")
+        else:
+            self.dest_name = dest_name
 
-	def checkname(self):
-			while True:
-				try:
-					filesource = open(self.src_file, 'rb').read()
-					return filesource
-				except:
-					print("Blad , wprowadz ponownie nazwy plikow z rozszerzeniami!")
-					self.zmienne()
-	def wysylanie(self,server):
-		self.zmienne()
-		c = self.checkname()
-		server.sendfile(self.dest_name, c)
-		return 1
+    def wysylanie(self, server):
 
+        filesource = open(self.src_file, 'rb').read()  # open ('function.py').read()
+        # wylacznie zaladowanie kodu na serwer
 
-	def moduly(self,server):
-		self.zmienne()
-		c = self.checkname()
-		#modulesource = open(self.src_file).read()
-		#wyslanie kodu JAKO MODUL (import):
-		server.sendmodule(self.dest_name, c)
-		#uruchomienie wyslanego kodu z podanymi argumentami
-		result = server.module(self.dest_name).doSomething("Marcin", 42)
-		print("\nresult from uploaded module:", result)
+        server.sendfile(self.dest_name, filesource)
 
-	def poprawnosc(self,server):
-		self.zmienne()    #czekaj wrzuc tutaj tylko zmiernna self.src_ zrodlowego...									##czekaj.... wez w tym file source daj plik 4funkcja,py
-		c = self.checkname()
-		root = server.evaluate(c)
-		print("result=", root)
-		
+        return 1
 
-	def funkcja(self,server):
-		number = 0
-		x = input("podaj nazwe pliku zrodlowego znajdujacego sie na serwerze ")
-		modulesource = open ('function.py').read()
-		#wyslanie kodu JAKO MODUL (import):
-		flame.sendmodule('flameexample.function', modulesource)
-		#uruchomienie wyslanego kodu z podanymi argumentami
-		result = flame.module('flameexample.function').numberoflines(x)
-		print("\nWybrany kod pliku przeslanego na serwer ma:", result, "linii kodu")
-		with open(x, 'r') as f:
-			for line in f:
-				number += 1
-		r = number
-		print ('kod żródłowy pliku zawiera:',r,'linii')
-		if r == result:
-			print ("Ilość linii kodu programu przesłanego na serwer jest równa ilości linii kodu źródłowego programu,", result,'=',r)
-		else: print("Ilosc linii kodu miedzy plikami na serwerze i kliencie rozni sie!")
+    def moduly(self):
+        self.zmienne()
+        modulesource = open(self.src_file).read()
+        # wyslanie kodu JAKO MODUL (import):
+        flame.sendmodule(self.dest_name, modulesource)
+        # uruchomienie wyslanego kodu z podanymi argumentami
+        result = flame.module(self.dest_name).doSomething("Marcin", 42)
+        print("\nresult from uploaded module:", result)
 
+    def poprawnosc(self):
+        self.zmienne()    #czekaj wrzuc tutaj tylko zmiernna self.src_ zrodlowego...									##czekaj.... wez w tym file source daj plik 4funkcja,py
+        filesource = open(self.src_file, 'rb').read()
+        root = flame.evaluate(filesource)
+        print("result=", root)
+        return root
+
+    def funkcja(self, server, filename):
+        modulesource = open('function.py').read()
+        # wyslanie kodu JAKO MODUL (import):
+        server.sendmodule('flameexample.function', modulesource)
+        # uruchomienie wyslanego kodu z podanymi argumentami
+        result = server.module('flameexample.function').numberoflines(filename)
+        print("\nWybrany program ma:", result, "lini kodu")
+        r = self.numberoflines(filename)
+        print('kod żródłowy zawiera:', r, 'linii')
+        if r == result:
+            print("Ilość linii kodu programu przesłanego na serwer jest równa ilości linii kodu źródłowego programu,",
+                  result, '=', r)
+
+        return result
 
 
 def main():
-	wybor = None
-	print ("Witaj w programie klienta")
-	while wybor != "1" and wybor != "2" and wybor !="3" and wybor != "4" and wybor != "5":
-		print ("Aby uruchomic funkcje klienta wybierz jedna z wybranych opcji: ")
-		
-		print ("1 - Wysylanie pliku do serwera")
-		print ("2 - Sprawdzenie poprawnosci kodu i kompilacja  ")
-		print ("3 - Wykonanie programu")
-		print ("4 - Raport")
-		print ("5 - Wyjscie z programu")
+    wybor = 0
+    print("Witaj programie klienta")
+    while wybor != 1 and wybor != 2 and wybor != 3 and wybor != 4 and wybor != 5:
+        print("Aby uruchomic funkcje klienta wybierz jedna z wybranych opcji: ")
 
-		wybor = str(input("Podaj numer: "))
-		if wybor != "1" and wybor != "2" and wybor != "3" and wybor != "4" and wybor != "5":
-			print("Niewłaściwy wybór. Spróbuj jeszcze raz")
-	#wyslanie pliku na serwer
-	if wybor == "1":
-		file.wysylanie(flame)
+        print("1 - Wysylanie pliku do serwera")
+        print("2 - Sprawdzenie poprawnosci kodu i kompilacja  ")
+        print("3 - Wykonanie programu")
+        print("4 - 4 funkcja")
+        print("5 - Wyjscie z programu")
 
+        wybor = int(input("Podaj numer: "))
+        if wybor != 1 and wybor != 2 and wybor != 3 and wybor != 5 and wybor != 4:
+            print("Niewłaściwy wybór. Spróbuj jeszcze raz")
+    # wyslanie pliku na serwer
 
-	# wysylanie kodu jako modul na serwer	
-	if wybor == "2":
-		file.moduly(flame)
+    if wybor == 1:
+        file.zmienne()
+        file.wysylanie(flame)
 
-	#sprawdzenie poprawnosci 
-	if wybor == "3":	
-		file.poprawnosc(flame)
-	# sprawdzenie poprawnosci
-	if wybor == "4":
-		file.funkcja(flame)
-	#wyjscie z menu
-	if wybor == "5":
-		print ("Dziękujemy za skorzystanie z naszych usług. Do zoboczenia!")
-		sys.exit(0)
+    # wysylanie kodu jako modul na serwer
+    if wybor == 2:
+        file.moduly()
+
+    # sprawdzenie poprawnosci
+    if wybor == 3:
+        file.zmienne()
+        file.poprawnosc()
+    # sprawdzenie poprawnosci
+    if wybor == 4:
+        file_name = input("podaj nazwe pliku znajdującego się na serwerze")
+        file.funkcja(flame, file_name)
+    # wyjscie z menu
+    if wybor == 5:
+        print("Dziękujemy za skorzystanie z naszych usług. Do zoboczenia!")
+        sys.exit(0)
+
 
 def koniec():
-	print("Dziękujemy za skorzystanie z naszych usług. Do widzenia! ")
-	return False
+    print("Dziękujemy za skorzystanie z naszych usług. Do widzenia! ")
+    return False
 
 
 if __name__ == '__main__':
-	if sys.version_info < (3, 0):
-		input = raw_input
+    if sys.version_info < (3, 0):
+        input = raw_input
 
-	Pyro4.config.SERIALIZER = "pickle"  # flame requires pickle serializer
+    Pyro4.config.SERIALIZER = "pickle"  # flame requires pickle serializer
 
-	print("Start a Pyro Flame server somewhere.")
-	def connection():
-		location = input("what is the location of the flame server, hostname:portnumber? ")
-		while True:
-			try:
-				flame = Pyro4.utils.flame.connect(location)
-				print()
-				return flame
-			except:
-				print('sproboj ponownie')
-				location = input("what is the location of the flame server, hostname:portnumber? ")
+    print("Start a Pyro Flame server somewhere.")
+    # location = input("what is the location of the flame server, hostname:portnumber? ")
+    location = "localhost:43863"
+    print()
 
+    # connect!
+    flame = Pyro4.utils.flame.connect(location)
 
-flame = connection()
+    # basic stuff
+    socketmodule = flame.module("socket")
+    osmodule = flame.module("os")
+    print("remote host name=", socketmodule.gethostname())
+    print("remote server current directory=", osmodule.getcwd())
 
+    file = Rmi()
+    # Informacje na serwerze gdy polaczy sie z klientem
+    flame.builtin("print")("Serwer polaczyl sie z klientem")
 
-# basic stuff
-socketmodule = flame.module("socket")
-osmodule = flame.module("os")
-print("remote host name=", socketmodule.gethostname())
-print("remote server current directory=", osmodule.getcwd())
+    while True:
+        main()
+        a = input("Czy chciałbyś jeszcze skorzystać z któreś usługi? (y/n)")
+        if a == 'y':
+            main()
+        elif a != 'y' and a != 'n':
+            print("Wybrano niewłaściwy klawisz")
+            a = input("Czy chciałbyś jeszcze skorzystać z któreś usługi? (y/n)")
 
-file = Rmi()
-# Informacje na serwerze gdy polaczy sie z klientem
-flame.builtin("print")("Serwer polaczyl sie z klientem")
-
-
-while True:
-	main()
-	a= input("Czy chciałbyś jeszcze skorzystać z któreś usługi? (y/n)")
-	if a =='y':
-		main()
-	elif a != 'y' and a != 'n':
-		print ("Wybrano niewłaściwy klawisz")
-		a = input("Czy chciałbyś jeszcze skorzystać z któreś usługi? (y/n)")
-
-	if a == "n":
-		print("Dziękujemy za skorzystanie z naszych usług. Do zoboczenia!")
-		sys.exit(0)
+        if a == "n":
+            print("Dziękujemy za skorzystanie z naszych usług. Do zoboczenia!")
+            sys.exit(0)
--- test_client.py
+++ test_client.py
@@ -1,7 +1,29 @@
 import client
+import function
 
 
-class Server_moc:
+class Model_mock:
+    def __init__(self, name):
+        pass
+
+    def numberoflines(self, filename):
+        return function.numberoflines(filename)
+
+
+
+
+class Server_mock:
+    def __init__(self):
+        self.filename = ''
+
+    def sendmodule(self, name, file):
+        self.filename = file
+
+    def module(self, name):
+        model = Model_mock(name)
+
+        return model
+
     def sendfile(self, plikserwer, filesource):
         pass
 
@@ -9,6 +31,16 @@ class Server_moc:
 def test_wysylanie():
     x = client.Rmi()
     x.zmienne('stuff.py', 't.py')
-    server = Server_moc()
+    server = Server_mock()
     res = x.wysylanie(server)
     assert res == 1
+
+
+def test_function():
+    x = client.Rmi()
+    server = Server_mock()
+    res = x.funkcja(server, 'function.py')
+    assert res == 13
+
+
+
